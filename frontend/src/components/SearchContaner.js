4283b4c4cfe3fc6378a53c2e7f19721c3f81e9e4
chan <sniwoo98@naver.com>
2023-03-31 00:03:40 +0900

    feat: SearchContainer 스타일 수정 및 Waypoint 추가

--- frontend/src/components/SearchContaner.js
+++ frontend/src/components/SearchContaner.js
@@ -6,10 +6,19 @@ const SearchContainer = () => {
   return (
     <div className="SearchContainer">
       <div>
-        <input type="text" placeholder="출발지를 입력하세요" />
+        <input
+          className="Waypoint"
+          type="text"
+          placeholder="출발지를 입력하세요"
+        />
       </div>
+
       <div>
-        <input type="text" placeholder="도착지를 입력하세요" />
+        <input
+          className="Waypoint"
+          type="text"
+          placeholder="도착지를 입력하세요"
+        />
       </div>
       <div>
         <Appbutton />
