a1061070cf55a6287e7dc66d90162fc189b55adf
chan <sniwoo98@naver.com>
2023-05-05 16:57:14 +0900

    feat : 카카오 길찾기 API 추가

---
+++ frontend/src/pages/Search/Search.js
@@ -0,0 +1,39 @@
+import React, {
+    useEffect, useState
+} from "react";
+import DaumPostcode from "react-daum-postcode";
+
+
+const Postcode = (props) => {
+  const complete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+    console.log(data);
+    console.log(fullAddress);
+    console.log(data.zonecode);
+
+    props.setcompany({
+      ...props.company,
+      address: fullAddress,
+    });
+  };
+
+  return (
+    <div>
+      <DaumPostcode className="postmodal" autoClose onComplete={complete} />
+    </div>
+  );
+};
+
+export default Postcode;
--- frontend/src/route/MainRoute.js
+++ frontend/src/route/MainRoute.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Main }from 'pages';
+import { Main, Search }from 'pages';
 
 export default function MainRoute() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="" element={<Main />} />
+        <Route path="Search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
