ffc7b908a92b433fdbff0c896a2a7d7768e1452d
chan <sniwoo98@naver.com>
2023-05-21 17:08:23 +0900

    feat: 헤더의 로고 누르면 홈으로 돌아오는 기능 추가

--- frontend/src/components/Appheader/Appheader.js
+++ frontend/src/components/Appheader/Appheader.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./Appheader.css";
+import { Link } from 'react-router-dom';
 
 function Appheader() {
   return (
+    <Link to={'./'}
+      style={{
+        textDecoration: "none"}}>
     <div className="Appheader">
       <h1>ALGOTA</h1>
-    </div>
+      </div>
+      </Link>
   );
 };
 export default Appheader;
